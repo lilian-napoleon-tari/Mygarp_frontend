e59b5cc65efd279441773aaa3a94bd992a24198e
Lilian Napoleon Modupe <napoleonliliantari@outlook.com>
2025-03-14 14:59:37 +0100

    feat: implemented the login page

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        deep_green: "#224F34",
+        light_green: "#C2EFD4",
+      },
+    },
+  },
+  plugins: [],
+};
